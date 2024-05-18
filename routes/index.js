80dc0d1abcab9efc1352dca8247d16b1f194481e
amit-vis <amit200698@gmail.com>
2024-05-18 22:43:10 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('../controller/home');
+const homeController = require('../controller/home_controller');
 
 router.get('/', homeController.home);
 router.use('/user', require('./user'));
